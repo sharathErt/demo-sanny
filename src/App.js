b27e5e82cbd05947534a0bd6b1bfd5d946f0f4e3
sharathErt <sharathepalapally@errortech.in>
2024-06-19 16:09:51 +0530

    img component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import ImgComp from './ImgComp';
 
 function App() {
 
@@ -10,17 +11,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <img src="/logo.png" className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ImgComp/>
       </header>
     </div>
   );
---
+++ src/ImgComp.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ImgComp = () => {
+  return (
+    <div>
+        <h1>img component </h1>
+        <img src='/logo.png' alt='logo' />
+    </div>
+  )
+}
+
+export default ImgComp
